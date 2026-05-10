a7aeffe387e467cd3be72b1fc668f28bada219e5
Diego Bardesio <141086093+BardesioDiego@users.noreply.github.com>
2026-05-10 17:49:53 -0300

    commit cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,8 +29,7 @@
 
   function mobileNavToogle() {
     document.querySelector('body').classList.toggle('mobile-nav-active');
-    mobileNavToggleBtn.classList.toggle('bi-list');
-    mobileNavToggleBtn.classList.toggle('bi-x');
+    mobileNavToggleBtn.classList.toggle('active');
   }
   if (mobileNavToggleBtn) {
     mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
